12efd3ed5f7e98c07e2d39c7510ed9d5dc61ad31
Afnan-Sayed <afnansayed50@gmail.com>
2025-08-23 15:10:00 +0300

    Finished Repos

--- spring-boot/src/main/java/com/ccs/Repositories/PasswordResetTokenRepository.java
+++ spring-boot/src/main/java/com/ccs/Repositories/PasswordResetTokenRepository.java
@@ -1,4 +1,12 @@
 package com.ccs.Repositories;
 
-public class PasswordResetTokenRepository {
+import com.ccs.Models.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface PasswordResetTokenRepository
+        extends JpaRepository<PasswordResetToken, Long>
+{
+    Optional<PasswordResetToken> findByToken(String token);
 }
+
--- spring-boot/src/main/java/com/ccs/Repositories/UserRepository.java
+++ spring-boot/src/main/java/com/ccs/Repositories/UserRepository.java
@@ -1,5 +1,11 @@
 package com.ccs.Repositories;
 
+import com.ccs.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
 // findByEmail(), existsByEmail()
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Long>
+{
+    Optional<User> findByEmail(String email);
 }
