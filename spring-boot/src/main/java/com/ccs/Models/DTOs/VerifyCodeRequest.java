86bee9f5270abed431540508f3580db6ecbd48b7
Afnan Sayed <afnansayed50@gmail.com>
2025-08-23 16:10:09 +0300

    Revert "Reset password models and repos"

--- spring-boot/src/main/java/com/ccs/Models/DTOs/VerifyCodeRequest.java
+++
@@ -1,7 +0,0 @@
-package com.ccs.Models.DTOs;
-
-public class VerifyCodeRequest
-{
-    private String email;
-    private String token;
-}
--- spring-boot/src/main/java/com/ccs/Repositories/UserRepository.java
+++ spring-boot/src/main/java/com/ccs/Repositories/UserRepository.java
@@ -1,11 +1,5 @@
 package com.ccs.Repositories;
 
-import com.ccs.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
 // findByEmail(), existsByEmail()
-public interface UserRepository extends JpaRepository<User, Long>
-{
-    Optional<User> findByEmail(String email);
+public class UserRepository {
 }
