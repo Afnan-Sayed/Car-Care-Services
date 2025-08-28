b6bdcf07cf89bc6965e36801be99ffd1779931a7
Ahmed Sabry <mohamedmahrous581@gmail.com>
2025-08-28 21:27:56 +0300

    add settings apis

--- spring-boot/src/main/java/com/ccs/Repository/SettingsRepo.java
+++ spring-boot/src/main/java/com/ccs/Repository/SettingsRepo.java
@@ -1,8 +1,17 @@
 package com.ccs.Repository;
 
+import com.ccs.Models.Settings;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 /*
 Rojeh
 for settings: find, update
  */
-public class SettingsRepo {
+@Repository
+public interface SettingsRepo extends JpaRepository<Settings, Long> {
+    // Since there should be only one settings record, we can use this method
+    Optional<Settings> findFirstByOrderById();
 }
