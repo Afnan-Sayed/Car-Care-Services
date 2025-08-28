993b8110cca5dd492242e6e49cf79ce5d11b2b31
Ahmed Sabry <mohamedmahrous581@gmail.com>
2025-08-28 19:22:45 +0300

    Implement enquiries apis

--- spring-boot/src/main/java/com/ccs/Repository/SettingsRepo.java
+++ spring-boot/src/main/java/com/ccs/Repository/SettingsRepo.java
@@ -1,7 +1,7 @@
 package com.ccs.Repository;
+
 /*
 Rojeh
-for enquiries: findAll, save
 for settings: find, update
  */
 public class SettingsRepo {
--- spring-boot/src/main/java/com/ccs/Services/SettingsService/SettingsService.java
+++ spring-boot/src/main/java/com/ccs/Services/SettingsService/SettingsService.java
@@ -1,11 +1,10 @@
 package com.ccs.Services.SettingsService;
+
 /*
 Rojeh
 Epic 10: Settings & Enquiries
 •	GET /settings
 •	PUT /settings
-•	GET /enquiries
-•	POST /enquiries
 
  */
 public class SettingsService {
