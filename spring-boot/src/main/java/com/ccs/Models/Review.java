f9e51a86534311bf07275f7fb4fea006ba9e35fd
Noureldin Shimi <122989841+NORO-01@users.noreply.github.com>
2025-08-29 21:40:38 +0300

    Added Review Model

--- spring-boot/src/main/java/com/ccs/Models/Review.java
+++ spring-boot/src/main/java/com/ccs/Models/Review.java
@@ -1,6 +1,33 @@
 package com.ccs.Models;
-/*
-id, request_id, customer_id, provider_id, rating (1–5), comment, created_at
- */
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "reviews")
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "order_id", nullable = false, unique = true)
+    private Order order;
+
+    @Column(name = "rating", nullable = false)
+    private int rating;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 }
